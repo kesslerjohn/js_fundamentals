630cf9d4938a724c3a4232c0880bb678f470f3cb
kesslerjohn <john-kessler@uiowa.edu>
2024-05-31 10:23:29 -0500

    add scorekeeping and game reset on win

--- memory_game/app.js
+++ memory_game/app.js
@@ -51,7 +51,8 @@ const cardArray = [
 
 const gridDisplay = document.querySelector('#grid')
 let score = 0;
-let flippedCard = null 
+const cardsChosen = []
+const cardsChosenIds = []
 
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
@@ -61,16 +62,54 @@ function createBoard() {
         card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
     }
-    
+    score = 0;
+    score_display.innerHTML = score
+}
+
+function checkMatch() {
+    const cards = document.querySelectorAll('#grid img')
+    if (cardsChosen[0] == cardsChosen[1]){
+        alert('match!')
+        score += 1
+        score_display.innerHTML = score
+        cardsChosenIds.forEach((id) => {
+            cards[id].setAttribute('src', 'images/white.png')
+            cards[id].removeEventListener('click', flipCard)
+        })
+        cardsChosen.length = 0
+        cardsChosenIds.length = 0
+        if (score === 6){
+            setTimeout(resetBoard, 1000)
+        }
+    } else {
+        alert('try again.')
+        cardsChosenIds.forEach((id) => {
+            cards[id].setAttribute('src', 'images/blank.png')
+        })
+        cardsChosen.length = 0
+        cardsChosenIds.length = 0
+    }
 }
 
 function flipCard() {
     const cardId = this.getAttribute('data-id')
-    this.setAttribute('src', cardArray[cardId]['img'])
-    
+    this.setAttribute('src', cardArray[cardId].img)
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkMatch, 500)
+    }
+}
+
+function resetBoard() {
+    alert('you win!')
+    while (gridDisplay.firstChild){
+        gridDisplay.removeChild(gridDisplay.lastChild)
+    }
+    cardArray.sort(() => 0.5 - Math.random())
+    createBoard()
 }
 
 cardArray.sort(() => 0.5 - Math.random())
 const score_display = document.getElementById('result')
-score_display.innerHTML = score
 createBoard()
