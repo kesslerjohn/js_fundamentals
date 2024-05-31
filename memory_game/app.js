b5ddab90cf51ffcc43787bd50b5e3e2eb93a416c
kesslerjohn <john-kessler@uiowa.edu>
2024-05-31 09:27:11 -0500

    show blank cards and styling

--- memory_game/app.js
+++ memory_game/app.js
@@ -48,3 +48,18 @@ const cardArray = [
         img: 'images/pizza.png'
     }
 ]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        gridDisplay.append(card)
+    }
+}
+
+createBoard()
