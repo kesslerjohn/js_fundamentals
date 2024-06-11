9e075a5a9d404df9ee6301ec13190dc473674107
kesslerjohn <john-kessler@uiowa.edu>
2024-06-11 15:56:43 -0500

    fill out game logic, add mole image

--- whac_a_mole/app.js
+++ whac_a_mole/app.js
@@ -5,7 +5,10 @@ const timeLeft = document.querySelector('#time-left')
 
 const score = document.querySelector("#score")
 
-let result = 0;
+let result = 0
+let hitPosition
+let currentTime = 10
+let timerId = null
 
 function randomSquare() {
     squares.forEach(square => {
@@ -14,4 +17,35 @@ function randomSquare() {
 
     let randomSquare = squares[Math.floor(Math.random() * 9)]
     randomSquare.classList.add('mole')
+
+    hitPosition = randomSquare.id
 }
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++
+            score.innerHTML = result
+            hitPosition = null
+        }
+    })
+})
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 500)
+}
+
+function countDown(){
+    currentTime--
+
+    if (currentTime == 0){
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert("GAME OVER! Final score: " + result)
+    }
+    timeLeft.innerHTML = currentTime
+}
+
+moveMole()
+
+let countDownTimerId = setInterval(countDown, 1000)
