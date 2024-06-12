de0613e4294fdf5e9aad078ef7bfeb6e99249b23
kesslerjohn <john-kessler@uiowa.edu>
2024-06-11 20:11:48 -0500

    add toby remix

--- whac_a_mole/app.js
+++ whac_a_mole/app.js
@@ -13,21 +13,34 @@ let timerId = null
 function randomSquare() {
     squares.forEach(square => {
         square.classList.remove('mole')
+        square.classList.remove('toby')
     })
 
     let randomSquare = squares[Math.floor(Math.random() * 9)]
-    randomSquare.classList.add('mole')
-
+    let tobyChance = Math.floor(Math.random()*5)
+    if (tobyChance == 3){
+        randomSquare.classList.add('toby')
+    } else {
+        randomSquare.classList.add('mole')
+    }
     hitPosition = randomSquare.id
 }
 
 squares.forEach(square => {
     square.addEventListener('mousedown', () => {
         if (square.id == hitPosition) {
-            result++
-            score.innerHTML = result
-            hitPosition = null
-        }
+            if (square.classList.contains('toby')) {
+                score.innerHTML = "-1,000,000"
+                alert("You bopped Toby :( You lose!")
+                clearInterval(countDownTimerId)
+                clearInterval(timerId)
+            } else {
+                result++
+                score.innerHTML = result
+                hitPosition = null
+            }
+            
+        } 
     })
 })
 
